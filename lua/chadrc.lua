060df43f5256ac1155b77510dd5281c5c0faffe7
Alexander Nepein <51252300+netrunner10@users.noreply.github.com>
2025-09-09 16:19:33 +0000

    Update allowed file templates for Kubernetes

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,7 +6,7 @@
 local M = {}
 
 M.base46 = {
-  theme = "monochrome",
+  theme = "vscode_dark",
   transparency = true,
 
   hl_override = {
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,7 +5,7 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jj", "<ESC>")
+map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
