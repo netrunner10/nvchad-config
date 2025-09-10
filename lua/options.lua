3c464fb169b6d3404583da2e784ffd153b103196
Alexander Nepein <51252300+netrunner10@users.noreply.github.com>
2025-09-10 20:21:30 +0000

    Add vim options

--- lua/options.lua
+++ lua/options.lua
@@ -6,6 +6,22 @@ local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 o.relativenumber = true
 
-o.expandtab = true
-o.softtabstop = 4
 o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+o.autoindent = true
+o.smartindent = true
+o.wrap = false
+
+o.swapfile = false
+o.backup = false
+o.undofile = true
+
+o.termguicolors = true
+o.background = "dark"
+o.scrolloff = 8
+o.signcolumn = "yes"
+
+o.updatetime = 50
+
