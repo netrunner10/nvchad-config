390040e563cdbfbd177d824f9082cc84df5429ab
Alexander Nepein <51252300+netrunner10@users.noreply.github.com>
2025-09-12 18:31:52 +0000

    Refactor lspconfig.lua

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,114 +4,111 @@ local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
-
-lspconfig.gopls.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-  settings = {
-    gopls = {
-      gofumpt = true,
-      codelenses = {
-        gc_details = false,
-        generate = true,
-        regenerate_cgo = true,
-        run_govulncheck = true,
-        test = true,
-        tidy = true,
-        upgrade_dependency = true,
-        vendor = true,
-      },
-      hints = {
-        assignVariableTypes = true,
-        compositeLiteralFields = true,
-        compositeLiteralTypes = true,
-        constantValues = true,
-        functionTypeParameters = true,
-        parameterNames = true,
-        rangeVariableTypes = true,
-      },
-      analyses = {
-        nilness = true,
-        unusedparams = true,
-        unusedwrite = true,
-        useany = true,
-      },
+local servers = {
+  gopls = {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+    settings = {
       usePlaceholders = true,
-      completeUnimported = true,
-      staticcheck = true,
-      directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+      gofumpt = true,
+      hints = {
+        parameterNames = true,
+        compositeLiteralFields = true,
+      },
     },
   },
-}
 
-lspconfig.yamlls.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = {
-    textDocument = {
-      foldingRange = {
-        dynamicRegistration = false,
-        lineFoldingOnly = true,
-      },
-    },
-  },
-  -- lazy-load schemastore when needed
-  on_new_config = function(new_config)
-    new_config.settings.yaml.schemas =
-      vim.tbl_deep_extend("force", new_config.settings.yaml.schemas or {}, require("schemastore").yaml.schemas())
-  end,
-  settings = {
-    redhat = { telemetry = { enabled = false } },
-    yaml = {
-      keyOrdering = false,
-      format = {
-        enable = true,
-      },
-      validate = { enable = true },
-      schemaStore = {
-        -- Must disable built-in schemaStore support to use
-        -- schemas from SchemaStore.nvim plugin
-        enable = false,
-        -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
-        url = "",
-      },
-      schemas = {
-        ["kubernetes"] = {
-          "kubectl-edit-*.yaml",
-          "*Role.yaml",
-          "*RoleBinding.yaml",
-          "*ClusterRole.yaml",
-          "*ClusterRoleBinding.yaml",
-          "*Pod.yaml",
-          "*ReplicaSet.yaml",
-          "*Deployment.yaml",
-          "*Service.yaml",
-          "*ServiceAccount.yaml",
-          "*Namespace.yaml",
-          "*Job.yaml",
-          "*CronJob.yaml",
-          "*ConfigMap.yaml",
-          "*Secret.yaml",
+  yamlls = {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = {
+      textDocument = {
+        foldingRange = {
+          dynamicRegistration = false,
+          lineFoldingOnly = true,
         },
-        ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-        ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-        ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-        ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*.gitlab-ci.{yml,yaml}",
+      },
+    },
+    -- lazy-load schemastore when needed
+    on_new_config = function(new_config)
+      new_config.settings.yaml.schemas =
+        vim.tbl_deep_extend("force", new_config.settings.yaml.schemas or {}, require("schemastore").yaml.schemas())
+    end,
+    settings = {
+      redhat = { telemetry = { enabled = false } },
+      yaml = {
+        keyOrdering = false,
+        format = {
+          enable = true,
+        },
+        validate = { enable = true },
+        schemaStore = {
+          -- Must disable built-in schemaStore support to use
+          -- schemas from SchemaStore.nvim plugin
+          enable = false,
+          -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+          url = "",
+        },
+        schemas = {
+          ["kubernetes"] = {
+            "kubectl-edit-*.yaml",
+            "*Role.yaml",
+            "*RoleBinding.yaml",
+            "*ClusterRole.yaml",
+            "*ClusterRoleBinding.yaml",
+            "*Pod.yaml",
+            "*ReplicaSet.yaml",
+            "*Deployment.yaml",
+            "*Service.yaml",
+            "*ServiceAccount.yaml",
+            "*Namespace.yaml",
+            "*Job.yaml",
+            "*CronJob.yaml",
+            "*ConfigMap.yaml",
+            "*Secret.yaml",
+          },
+          ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+          ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+          ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+          ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*.gitlab-ci.{yml,yaml}",
+        },
+      },
+    },
+  },
+
+  dockerls = {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  },
+
+  docker_compose_language_service = {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  },
+
+  jsonls = {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+    on_new_config = function(new_config)
+      new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+      vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
+    end,
+    settings = {
+      json = {
+        format = {
+          enable = true,
+        },
+        validate = { enable = true },
       },
     },
   },
 }
 
-lspconfig.dockerls.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
-
-lspconfig.docker_compose_language_service.setup {
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
-}
+for name, opts in pairs(servers) do
+  vim.lsp.config(name, opts)
+  vim.lsp.enable(name)
+end
