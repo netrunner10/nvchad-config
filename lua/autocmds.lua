60597fec2468ec93f1684510e8e1ed8163e916d2
Alexander Nepein <51252300+netrunner10@users.noreply.github.com>
2025-09-12 18:30:43 +0000

    Sparate tabsize & Add NvDashboard on empty buffers

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,21 @@
 require "nvchad.autocmds"
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("BufEnter", {
+  pattern = { "*.html", "*.css", "*.yaml", "*.yml", "*.json", "*lua", "*.js" },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufDelete", {
+  callback = function()
+    local bufs = vim.t.bufs
+    if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
+      vim.cmd "Nvdash"
+    end
+  end,
+})
