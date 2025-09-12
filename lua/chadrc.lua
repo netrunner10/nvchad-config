09f9265faee2895c200204f5fd1a4bb6b1829bb7
Alexander Nepein <nepein619@gmail.com>
2025-09-13 03:44:52 +0500

    Add NvDash headers

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,6 +1,7 @@
 -- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
 -- Please read that file to know all available options :(
+require "lua.nvdash_headers"
 
 ---@type ChadrcConfig
 local M = {}
@@ -15,7 +16,10 @@ M.base46 = {
   },
 }
 
-M.nvdash = { load_on_startup = true }
+M.nvdash = {
+  load_on_startup = true,
+  header = Nvdash_headers[math.random(1, #Nvdash_headers)],
+}
 M.ui = {
   tabufline = {
     lazyload = false,
